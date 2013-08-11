b4e96f85a9c719a9140cf87c7b2140176a44dc7d
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-08-11 20:07:59 +0100

    Adding xml to html script

---
+++ dochtml.py
@@ -0,0 +1,20 @@
+
+import xml.etree.ElementTree as ET
+
+def PreprocessXml(root):
+	
+	for elnum in range(len(root)):
+		print root[elnum].tag
+		el = root[elnum]
+		if el.tag == "include":
+			tree = ET.parse(el.attrib['file'])
+			root[elnum] = tree.getroot()
+
+if __name__ == "__main__":
+	
+	tree = ET.parse('thesis.xml')
+	root = tree.getroot()
+
+	root2 = PreprocessXml(root)
+
+	
